959de790d93133b4022df1c4aa11aaf18071be38
Svetlana Gorbunova <sviet_ka@mail.ru>
2020-05-08 23:42:15 -0700

    'Welcome!' kata is added

---
+++ Welcome.js
@@ -0,0 +1,23 @@
+//https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/javascript
+function greet(language) {
+    let database = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+    return ([language] in database) ? `${database[language]}` : "Welcome";
+}

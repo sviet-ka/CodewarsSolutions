dd0b6a414778fcb20b9cdec9c5de575467ec2170
Svetlana Gorbunova <sviet_ka@mail.ru>
2020-05-08 23:12:02 -0700

    'What's wrong with these identifiers?' kata is added

---
+++ What's wrong with these identifiers.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/56bb01de0e8b29de50000b19/train/javascript
+var Person = {
+    firstName: "John",
+    secondName: "Doe",
+    emailAddress: "john.doe@email.com",
+    male_female: "M"
+};
